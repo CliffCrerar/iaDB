27d91b12d9d7babcdaf925812d2fcd57f13a7a54
CliffCrerar <cliff.crerar@gmail.com>
2019-03-25 23:34:44 +0200

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+
+module.exports = {
+	entry: { app: "./src/index.js" },
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist"
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: "iaDB"
+		}),
+		new ManifestPlugin()
+	]
+};
